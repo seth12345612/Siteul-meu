9bf4db5afc7518a029980f7c7ea296661d244b2b
seth12345612 <anaaoglagioaie@gmail.com>
2026-04-30 12:33:51 +0300

    d

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, 'backend', '.env') });
 const crypto = require('crypto');
 const express = require('express');
 const bodyParser = require('body-parser');
